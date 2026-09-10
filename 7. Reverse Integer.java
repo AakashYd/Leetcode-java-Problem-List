9b90b3bb68f7c0fd957bf4321d3dc6ceaf06c8df
Aakash Yadav <ay9878169@gmail.com>
2026-09-10 15:49:45 +0530

    Add files via upload

---
+++ 7. Reverse Integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            int newRev = rev * 10 + digit;
+            if ((newRev - digit) / 10 != rev) {
+                return 0;
+            }
+
+            rev = newRev;
+            x /= 10;
+        }
+        return rev;
+    }
+}
