ab60d6187a31ae5046519fd491f7952575eb05f8
Aakash Yadav <ay9878169@gmail.com>
2026-09-12 16:52:18 +0530

    Add files via upload

---
+++ 9. Palindrome number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Negative numbers and numbers ending with 0 (except 0 itself) are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            int digit = x % 10;
+            reversedHalf = reversedHalf * 10 + digit;
+            x /= 10;
+        }
+
+        // For even length numbers: x == reversedHalf
+        // For odd length numbers: x == reversedHalf/10 (middle digit ignored)
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+}
